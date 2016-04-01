07f242ab5b112f5cd7239ca21baf6e97d2703b3c
Jason Shin <jasonshin98@gmail.com>
2016-04-01 09:30:45 -0400

    notes

--- hw06/lab.py
+++ hw06/lab.py
@@ -41,8 +41,23 @@ least one number.
 """
 def checkPass(password):
     UC_LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    [ for char in password if char in UC_LETTERS or char in 
-
+    LC_LETTERS=UC_LETTERS.lower()
+    upper = [1 if char in UC_LETTERS else 0 for char in password]
+    lower = [1 if char in LC_LETTERS else 0 for char in password]
+    number =[1 if char.isdigit() else 0 for char in password]
+    return True if 1 in upper and 1 in lower and 1 in number else False
+    
+print("checkPass(\"Hello\")")
+print(checkPass("Hello"))
+print("checkPass(\"12345\")")
+print(checkPass("12345"))
+print("checkPass(\"helloworld\")")
+print(checkPass("helloworld"))
+print("checkPass(\"12345HI\")")
+print(checkPass("12345HI"))
+print("checkPass(\"HelloWorld1\")")
+print(checkPass("HelloWorld1"))
+    
 """
   YOUR TASK The Second:
   Write a function that uses list comprehension to return a password's strength rating\
@@ -53,3 +68,28 @@ def checkPass(password):
    * inclusion of numerals
    * inclusion of these non-alphanumeric chars: . ? ! & # , ; : - _ *
 """
+
+def checkStrength(password):
+    UC_LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    LC_LETTERS=UC_LETTERS.lower()
+    SPEC_CHARS = ".?!&#,;:-_*"
+    passLen = len(password)
+    if passLen == 1:
+        return 1;
+    score = 0
+    upper = [1 for char in password if char in UC_LETTERS]
+    lower = [1 for char in password if char in LC_LETTERS]
+    number = [1 for char in password if char.isdigit()]
+    spec = [1 for char in password if char in SPEC_CHARS]
+    return float(len(upper) + len(number) + len(spec)) / passLen * 10  
+
+print("checkStrength(\"Hello\")")
+print(checkStrength("Hello"))
+print("checkStrength(\"12345\")")
+print(checkStrength("12345"))
+print("checkStrength(\"helloworld\")")
+print(checkStrength("helloworld"))
+print("checkStrength(\"12345HI\")")
+print(checkStrength("12345HI"))
+print("checkStrength(\"HelloWorld1\")")
+print(checkStrength("HelloWorld1"))
