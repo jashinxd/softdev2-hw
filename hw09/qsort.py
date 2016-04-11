33c8674dc85222274e6ad5165668f460bc7bdd0b
Jason Shin <jasonshin98@gmail.com>
2016-04-11 12:22:14 -0400

    better qsort

--- hw09/qsort.py
+++ hw09/qsort.py
@@ -1,12 +1,14 @@
+import random
+
 def qsort(l):
     # Check if length of list == 0
     if (len(l) <= 0):
         return l;
     # Pivot is l[0]
-    pivot = l[0]
+    pivot = random.choice(l)
     # Recursively sort
-    lower = qsort([x for x in l[1:] if x <= pivot])
-    upper = qsort([x for x in l[1:] if x > pivot])
-    return lower + [l[0]] + upper
+    lower = qsort([x for x in l if x < pivot])
+    upper = qsort([x for x in l if x > pivot])
+    return lower + [pivot] * l.count(pivot) + upper
 
-print qsort([9,2,3,5,1,3,1])
+print qsort([9,9,9,2,3,5,1,3,1])
